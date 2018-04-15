6c48fff4adbe2354d4a70d1f5826208f5ac7cc0f
LMalko <lmalko2601@gmail.com>
2018-04-15 21:07:39 +0200

    Add ending info print in main.py

--- 00 Clint Eastwood Web Scraping/main.py
+++ 00 Clint Eastwood Web Scraping/main.py
@@ -8,6 +8,7 @@ def main():
     except IndexError:
         actor_IMBD_home = "http://www.imdb.com/name/nm0000142/?ref_=nv_sr_1".split("?")[0]
     app = AppController(actor_IMBD_home)
+    print("\nData was successfully saved in .csv file.")
 
 
 if __name__ == "__main__":
