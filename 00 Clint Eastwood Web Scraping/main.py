e1c98063d31721b4dc2e9380454ce33dc69385ff
LMalko <lmalko2601@gmail.com>
2018-04-15 17:34:43 +0200

    Clean code main.py

--- 00 Clint Eastwood Web Scraping/main.py
+++ 00 Clint Eastwood Web Scraping/main.py
@@ -2,7 +2,8 @@ from Controller.AppController import *
 
 
 def main():
-    app = AppController()
+    clint_eastwood_IMBD_home = "https://www.imdb.com/name/nm0000142/"
+    app = AppController(clint_eastwood_IMBD_home)
 
 
 if __name__ == "__main__":
