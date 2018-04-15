3e82e1522e1705329e22be5703cede18064dabae
LMalko <lmalko2601@gmail.com>
2018-04-16 00:16:10 +0200

    Make methods in AppController private wherever possible

--- 00 Clint Eastwood Web Scraping/Controller/AppController.py
+++ 00 Clint Eastwood Web Scraping/Controller/AppController.py
@@ -8,30 +8,31 @@ class AppController:
     movie_collection = []
 
     def __init__(self, link):
-        response = self.check_url_response(link)
-        soup = BeautifulSoup(response.text, "html.parser")
-        self.set_movies_collection(soup)
+        self.response = self.check_url_response(link)
+        soup = BeautifulSoup(self.response.text, "html.parser")
+        self._set_movies_collection(soup)
 
         self.actors_name = self._set_actors_name(soup)
 
-        self.write_collection_to_csv_file(self.movie_collection)
+        self._write_collection_to_csv_file(self.movie_collection)
 
     @staticmethod
     def check_url_response(link):
         response = requests.get(link)
+        print(response)
         return response
 
-    def set_movies_collection(self, soup):
-        movies = self.get_movies(soup)
+    def _set_movies_collection(self, soup):
+        movies = self._get_movies(soup)
         for movie in movies:
-            temp_movie = self.set_movie_object(movie)
+            temp_movie = self._set_movie_object(movie)
             self.movie_collection.append(temp_movie)
 
     @staticmethod
-    def get_movies(soup):
+    def _get_movies(soup):
         return soup.findAll('div', id=lambda x: x and x.startswith('actor-'))
 
-    def write_collection_to_csv_file(self, collection):
+    def _write_collection_to_csv_file(self, collection):
         with open(f"{self.get_actors_name()} Movies.csv", "w", encoding="utf-8", newline='') as file:
             csv_writer = writer(file)
             csv_writer.writerow(["Title", "Character", "Year", "Category"])
@@ -39,7 +40,7 @@ class AppController:
                 csv_writer.writerow([item.title, item.character_played, item.release_year, item.category])
 
     @staticmethod
-    def set_movie_object(movie):
+    def _set_movie_object(movie):
         title = movie.find("a").text
         try:
             character = movie.find("b").next_sibling.next_sibling.next_sibling.split("\n")[1]
@@ -61,4 +62,7 @@ class AppController:
 
     def get_actors_name(self):
         return self.actors_name
+
+    def get_movies_collection(self):
+        return self.movie_collection
 
