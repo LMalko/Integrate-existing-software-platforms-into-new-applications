e8cce88a80876c63098a32050fc8780a65808751
LMalko <lmalko2601@gmail.com>
2018-04-15 15:08:12 +0200

    Set film attributes in app.js

--- 00 Clint Eastwood Web Scraping/Controller/app.py
+++ 00 Clint Eastwood Web Scraping/Controller/app.py
@@ -6,18 +6,29 @@ def check_url_response():
     response = requests.get ( "https://www.imdb.com/name/nm0000142/" )
     return response
 
-def get_films(soup):
+def get_movies(soup):
     return soup.findAll('div', id=lambda x: x and x.startswith('actor-'))
+
+def set_movies_attributes(movies):
+    for movie in movies:
+        title = movie.find("a").text
+        character = movie.find("b").next_sibling.next_sibling.next_sibling.split("\n")[1]
+        release_form = movie.find( "b" ).next_sibling
+
+        if release_form == "\n":
+            release_form = "Movie"
+        else:
+            release_form = release_form.split("\n")[1]
+
+        release_year = movie.find("span").text.split("\n")[1]
+        print(f"{title}***{character}***{release_form}***{release_year}")
+
 
 def start_app():
     response = check_url_response()
     soup = BeautifulSoup(response.text, "html.parser")
-    films = get_films(soup)
-    for film in films:
-        title = film.find("a").text
-        character = film.find("b").next_sibling.next_sibling.next_sibling
-        release_form = film.find( "b" ).next_sibling
-        release_year = film.find("span").text
+    movies = get_movies(soup)
+    set_movies_attributes(movies)
 
 
 
