a6147b0a9284f85f763d8882e5a25937c6c04dcf
LMalko <lmalko2601@gmail.com>
2018-04-23 20:31:53 +0200

    Fix appController instance in tests.py

--- 00 Clint Eastwood Web Scraping/tests.py
+++ 00 Clint Eastwood Web Scraping/tests.py
@@ -4,10 +4,14 @@ from Model.Movie import Movie
 
 class AppControllerTests(unittest.TestCase):
 
-    app_controller = AppController ( "http://www.imdb.com/name/nm0000142/" )
-
     def test_response(self):
-        self.assertEqual(str(self.app_controller.response), "<Response [200]>")
+        app_controller = AppController ( "http://www.imdb.com/name/nm0000142/" )
+        self.assertEqual(str(app_controller.get_response()), "<Response [200]>")
 
     def test_movie_collection(self):
-        self.assertTrue(all(isinstance(item, Movie) for item in self.app_controller.get_movies_collection()))
+        app_controller = AppController("http://www.imdb.com/name/nm0000143/")
+        self.assertTrue(all(isinstance(item, Movie) for item in app_controller.get_movies_collection()))
+
+
+if __name__ == '__main__':
+    unittest.main()
