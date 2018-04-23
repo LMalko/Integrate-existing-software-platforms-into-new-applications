ab0aeb40d32ba0db546311430722093e00229c07
LMalko <lmalko2601@gmail.com>
2018-04-23 20:32:14 +0200

    Add print and method call in main.py

--- 00 Clint Eastwood Web Scraping/main.py
+++ 00 Clint Eastwood Web Scraping/main.py
@@ -1,15 +1,15 @@
 from Controller.AppController import *
 from sys import argv
 
-
 def main():
     try:
-        actor_IMBD_home = argv[1].split ( "?" )[0]
+        actor_IMBD_home = argv[1].split("?")[0]
     except IndexError:
         actor_IMBD_home = "http://www.imdb.com/name/nm0000142/?ref_=nv_sr_1".split("?")[0]
     app = AppController(actor_IMBD_home)
+    print(app.get_response())
+    app.run_app()
     print("\nData was successfully saved in .csv file.")
-
 
 if __name__ == "__main__":
     main()
