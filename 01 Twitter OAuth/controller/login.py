1950eb0b138d37f3ae2f363a1df7537979a60657
LMalko <lmalko2601@gmail.com>
2018-05-11 01:45:02 +0200

    Write Login class in login.py

---
+++ 01 Twitter OAuth/controller/login.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append("..")
+from enums.constants import Constants
+import oauth2
+
+
+class Login:
+
+    consumer = oauth2.Consumer(Constants.CONSUMER_KEY.value,
+                                      Constants.CONSUMER_SECRET.value)
+    client = oauth2.Client(consumer)
+
+    def __init__(self):
+        self.response, self.content = self.client.request(Constants.REQUEST_TOKEN_URL.value, "POST")
+
+    def get_response_status(self):
+        return self.response.status
+
+    def get_content(self):
+        return self.content
