3b500a9abd5e549f6da90abbb99826b888fd2b02
LMalko <lmalko2601@gmail.com>
2018-05-11 13:56:18 +0200

    Write get_client in app.py

--- 01 Twitter OAuth/controller/app.py
+++ 01 Twitter OAuth/controller/app.py
@@ -4,6 +4,7 @@ from view.view import View
 from controller.login import Login
 import urllib.parse as urlparse
 from enums.constants import Constants
+import oauth2
 
 class App:
 
@@ -17,8 +18,22 @@ class App:
         request_token = dict(urlparse.parse_qsl(
             login.get_content().decode("utf-8")))
 
+        client = self.get_client(request_token)
+
+
+    def get_client(self, request_token):
+
         self.view.display_message("\nGo to the following site: \n")
         self.view.display_message(
             f"{Constants.AUTHORIZATION_URL.value}?oauth_token={request_token['oauth_token']}")
 
         authorization_verifier = self.view.get_user_input("What is the PIN? --> ")
+
+        token = oauth2.Token(request_token['oauth_token'],
+                             request_token['oauth_token_secret'])
+
+        token.set_verifier(authorization_verifier)
+
+        client = oauth2.Client(self.consumer, token)
+
+        return client
