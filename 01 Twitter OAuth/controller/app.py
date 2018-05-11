054a14c8bb33441c793008ede459ca418c438166
LMalko <lmalko2601@gmail.com>
2018-05-11 14:04:39 +0200

    Add get_request_token() % get_access_token() in app.py

--- 01 Twitter OAuth/controller/app.py
+++ 01 Twitter OAuth/controller/app.py
@@ -9,16 +9,23 @@ import oauth2
 class App:
 
     view = View()
+    login = Login()
 
 
     def start_app(self):
-        login = Login()
-        if login.get_response_status() != 200:
+
+        request_token = self.get_request_token()
+        client = self.get_client(request_token)
+        access_token = self.get_access_token(client)
+
+
+    def get_request_token(self):
+        if self.login.get_response_status() != 200:
             self.view.display_message("An error occured")
         request_token = dict(urlparse.parse_qsl(
-            login.get_content().decode("utf-8")))
+            self.login.get_content().decode("utf-8")))
 
-        client = self.get_client(request_token)
+        return request_token
 
 
     def get_client(self, request_token):
@@ -34,6 +41,14 @@ class App:
 
         token.set_verifier(authorization_verifier)
 
-        client = oauth2.Client(self.consumer, token)
+        client = oauth2.Client(self.login.get_consumer(), token)
 
         return client
+
+    def get_access_token(self, client):
+
+        request, content = client.request(Constants.ACCESS_TOKEN_URL.value, "POST")
+        access_token = dict(urlparse.parse_qsl(content.decode("utf-8")))
+
+        return access_token
+
