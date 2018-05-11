1ac447ce0e1be72a7d2f6edc15588ec4f9537ac1
LMalko <lmalko2601@gmail.com>
2018-05-11 14:52:51 +0200

    Refactor login.py

--- 01 Twitter OAuth/controller/login.py
+++ 01 Twitter OAuth/controller/login.py
@@ -2,22 +2,60 @@ import sys
 sys.path.append("..")
 from enums.constants import Constants
 import oauth2
+import urllib.parse as urlparse
 
 
 class Login:
 
-    consumer = oauth2.Consumer(Constants.CONSUMER_KEY.value,
-                                      Constants.CONSUMER_SECRET.value)
-    client = oauth2.Client(consumer)
+    @staticmethod
+    def get_consumer():
+        # Create a consumer, which uses CONSUMER_KEY and
+        # CONSUMER_SECRET to identify our app uniquely.
+        consumer = oauth2.Consumer(Constants.CONSUMER_KEY.value,
+                                          Constants.CONSUMER_SECRET.value)
+        return consumer
 
-    def __init__(self):
-        self.response, self.content = self.client.request(Constants.REQUEST_TOKEN_URL.value, "POST")
+    @staticmethod
+    def get_client(consumer):
+        client = oauth2.Client(consumer)
+        return client
 
-    def get_response_status(self):
-        return self.response.status
+    @staticmethod
+    def get_request(client):
+        # Use the client to perform a request for the request token.
+        return client.request(Constants.REQUEST_TOKEN_URL.value, "POST" )
 
-    def get_content(self):
-        return self.content
+    @staticmethod
+    def get_response_status(response):
+        return response.status
 
-    def get_consumer(self):
-        return self.consumer
+    @staticmethod
+    def get_request_token(content):
+        # Get the request token parsing the query string.
+        request_token = dict(urlparse.parse_qsl(content.decode( "utf-8")))
+        return request_token
+
+    @staticmethod
+    def get_verified_client(consumer, request_token, authorization_verifier):
+
+        # Create a Token object which contains the request token,
+        # and the verifier.
+        token = oauth2.Token(request_token['oauth_token'],
+                             request_token['oauth_token_secret'])
+
+        token.set_verifier(authorization_verifier)
+
+        # Create a client with the consumer (our app) and the newly
+        # created (and verified) token.
+        client = oauth2.Client(consumer, token)
+
+        return client
+
+    @staticmethod
+    def get_access_token(client):
+
+        # Ask Twitter for an access token after the request token was verified.
+        response, content = client.request(Constants.ACCESS_TOKEN_URL.value, "POST")
+        access_token = dict(urlparse.parse_qsl(content.decode("utf-8")))
+
+        return access_token
