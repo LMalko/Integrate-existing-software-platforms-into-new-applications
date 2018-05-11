a0a0ceb8e411aa087297d7745af65bc84b852260
LMalko <lmalko2601@gmail.com>
2018-05-11 14:52:40 +0200

    Refactor app.py

--- 01 Twitter OAuth/controller/app.py
+++ 01 Twitter OAuth/controller/app.py
@@ -2,9 +2,9 @@ import sys
 sys.path.append("..")
 from view.view import View
 from controller.login import Login
-import urllib.parse as urlparse
 from enums.constants import Constants
-import oauth2
+import urllib.parse as urlparse
+
 
 class App:
 
@@ -14,41 +14,33 @@ class App:
 
     def start_app(self):
 
-        request_token = self.get_request_token()
-        client = self.get_client(request_token)
-        access_token = self.get_access_token(client)
+        consumer = self.login.get_consumer()
 
+        client = self.login.get_client(consumer)
 
-    def get_request_token(self):
-        if self.login.get_response_status() != 200:
+        response, content = self.login.get_request(client)
+
+        if self.login.get_response_status(response) != 200:
             self.view.display_message("An error occured")
-        request_token = dict(urlparse.parse_qsl(
-            self.login.get_content().decode("utf-8")))
 
-        return request_token
+        request_token = self.login.get_request_token(content)
 
+        authorization_verifier = self.get_authorization_verifier(request_token)
 
-    def get_client(self, request_token):
+        client = self.login.get_verified_client(consumer, request_token, authorization_verifier)
 
+        access_token = self.login.get_access_token(client)
+        print(access_token)
+
+    def get_authorization_verifier(self, request_token):
+        # Ask the user to authorize app and give the pin code.
         self.view.display_message("\nGo to the following site: \n")
         self.view.display_message(
             f"{Constants.AUTHORIZATION_URL.value}?oauth_token={request_token['oauth_token']}")
 
         authorization_verifier = self.view.get_user_input("What is the PIN? --> ")
 
-        token = oauth2.Token(request_token['oauth_token'],
-                             request_token['oauth_token_secret'])
+        return authorization_verifier
 
-        token.set_verifier(authorization_verifier)
 
-        client = oauth2.Client(self.login.get_consumer(), token)
-
-        return client
-
-    def get_access_token(self, client):
-
-        request, content = client.request(Constants.ACCESS_TOKEN_URL.value, "POST")
-        access_token = dict(urlparse.parse_qsl(content.decode("utf-8")))
-
-        return access_token
 
