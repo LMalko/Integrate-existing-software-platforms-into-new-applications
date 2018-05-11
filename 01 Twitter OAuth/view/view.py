6a75999597f51e134760bdcfa9c9110ec0c972b4
LMalko <lmalko2601@gmail.com>
2018-05-11 13:43:11 +0200

    Add get_user_input in view.py

--- 01 Twitter OAuth/view/view.py
+++ 01 Twitter OAuth/view/view.py
@@ -2,3 +2,7 @@ class View:
 
     def display_message(self, message):
         print(message)
+
+    def get_user_input(self, message):
+        message = '\n' + message
+        return input(message)
