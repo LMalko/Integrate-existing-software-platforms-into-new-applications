032e2464e755336d73cd854c9c6961b3f9cce0eb
LMalko <lmalko2601@gmail.com>
2018-05-11 13:43:26 +0200

    Add input PIN in app.py

--- 01 Twitter OAuth/controller/app.py
+++ 01 Twitter OAuth/controller/app.py
@@ -17,6 +17,8 @@ class App:
         request_token = dict(urlparse.parse_qsl(
             login.get_content().decode("utf-8")))
 
-        self.view.display_message("Go to the following site: \n")
+        self.view.display_message("\nGo to the following site: \n")
         self.view.display_message(
             f"{Constants.AUTHORIZATION_URL.value}?oauth_token={request_token['oauth_token']}")
+
+        authentication_verifier = self.view.get_user_input("What is the PIN?")
