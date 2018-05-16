61af4f3faaaa1a49173df9dc997a86a4c63c3bfa
LMalko <lmalko2601@gmail.com>
2018-05-16 14:35:12 +0200

    Refactor instance attributes in user.py

--- 01 Twitter OAuth/model/user.py
+++ 01 Twitter OAuth/model/user.py
@@ -4,14 +4,12 @@ from model.database import *
 
 class User:
 
-    def __init__(self, id, first_name, last_name, email,
-                 oauth_token, oauth_token_secret):
+    def __init__(self, id, screen_name, oauth_token, oauth_token_secret):
         self.id = id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
+        self.screen_name = screen_name
         self.oauth_token = oauth_token
         self.oauth_token_secret = oauth_token_secret
+
 
     def get_oauth_token(self):
         return self.oauth_token
@@ -20,26 +18,24 @@ class User:
         return self.oauth_token_secret
 
     def __repr__(self):
-        return f"{self.first_name} {self.last_name} has id " \
-                                        f"{self.id} & email {self.email}."
+        return f"User: {self.screen_name}"
 
     def save_to_db(self):
 
         with CursorFromConnectionFromPool() as cursor:
 
-                cursor.execute ( "INSERT INTO users (first_name, last_name, "
-                                 "email, oauth_token, oauth_token_secret) "
-                                 "VALUES (%s, %s, %s, %s, %s);", (self.first_name, self.last_name,
-                                                          self.email, self.oauth_token, self.oauth_token_secret))
+                cursor.execute ( "INSERT INTO users (screen_name, oauth_token, "
+                                                            "oauth_token_secret) "
+                                 "VALUES (%s, %s, %s);", (self.screen_name,
+                                                            self.oauth_token, self.oauth_token_secret))
 
     @classmethod
-    def load_from_db_by_email(cls, email):
+    def load_from_db_by_screen_name(cls, screen_name):
         with CursorFromConnectionFromPool() as cursor:
 
-                cursor.execute("SELECT * FROM users WHERE email=%s", (email,))
+                cursor.execute("SELECT * FROM users WHERE screen_name=%s", (screen_name,))
 
                 user_data = cursor.fetchone()
                 if user_data is not None:
-                    return cls(id=user_data[0], first_name=user_data[1],
-                               last_name=user_data[2], email=user_data[3],
-                               oauth_token=user_data[4], oauth_token_secret=user_data[5])
+                    return cls(id=user_data[0], screen_name=user_data[1],
+                               oauth_token=user_data[2], oauth_token_secret=user_data[3])
